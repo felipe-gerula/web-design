68c94262b074d0819a848c55a543a466325fc43f
felipe-gerula <felipe.gerula@gmail.com>
2020-07-27 14:42:44 -0300

    actualización scroll reveal

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector(".menu-btn").addEventListener("click", ()=>{
     document.querySelector(".nav-main-left").classList.toggle("show");
 });
 
-ScrollReveal().reveal(".showcase");
-ScrollReveal().reveal(".novedades", {delay : 200});
-ScrollReveal().reveal(".novedades-banner", {delay : 200});
-ScrollReveal().reveal(".social", {delay : 200});
+ScrollReveal().reveal(".showcase", {delay : 300});
+ScrollReveal().reveal(".novedades", {delay : 400});
+ScrollReveal().reveal(".novedades-banner", {delay : 400});
+ScrollReveal().reveal(".social", {delay : 400});
