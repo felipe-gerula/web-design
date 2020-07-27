c8809ae5d3391f8b66461992b2df950c1d6b4290
felipe-gerula <felipe.gerula@gmail.com>
2020-07-27 14:16:21 -0300

    añadimos scroll reveal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 document.querySelector(".menu-btn").addEventListener("click", ()=>{
     document.querySelector(".nav-main-left").classList.toggle("show");
 });
+
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".novedades", {delay : 200});
+ScrollReveal().reveal(".novedades-banner", {delay : 200});
+ScrollReveal().reveal(".social", {delay : 200});
