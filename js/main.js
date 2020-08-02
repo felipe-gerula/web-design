16a244897a4832a22a46df120df47dfc32217f52
felipe-gerula <felipe.gerula@gmail.com>
2020-08-02 00:38:57 -0300

    formulario para contactar con el cliente

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ document.querySelector(".menu-btn").addEventListener("click", ()=>{
     document.querySelector(".nav-main-left").classList.toggle("show");
 });
 
-ScrollReveal().reveal(".showcase", {delay : 300});
-ScrollReveal().reveal(".novedades", {delay : 400});
-ScrollReveal().reveal(".novedades-banner", {delay : 400});
-ScrollReveal().reveal(".social", {delay : 400});
+ScrollReveal().reveal(".showcase", {delay : 400});
+ScrollReveal().reveal(".novedades", {delay : 500});
+ScrollReveal().reveal(".novedades-banner", {delay : 500});
+ScrollReveal().reveal(".social", {delay : 500});
+ScrollReveal().reveal(".form-container", {delay : 500});
